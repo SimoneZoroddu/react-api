689ad6fb41f2761b2ef71a0b2a8ca615d424f1a9
SimoneZoroddu <lenviur@gmail.com>
2026-03-17 13:05:13 +0100

    add in page the actresses

--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,21 +1,44 @@
+import { useEffect, useState } from "react";
 
 
 
-export default function AppMain(){
+export default function AppMain() {
+    const apiActresses_url = 'https://lanciweb.github.io/demo/api/actors/'
+    const [actresses, setActresses] = useState([])
 
 
 
+    function getActresses() {
 
-    
+
+        fetch(apiActresses_url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setActresses(data)
+            })
+    }
+
+    useEffect(() => { getActresses() }, [])
+
 
     return (
         <>
-        
-        
 
+            {
+                actresses?.map(actress => (
+                    <div key={actress.id}>
+                        <img src={actress.image} alt={actress.name} />
+                        <div>
+                            {actress.name}
+                        </div >
+                        <div>
+                            {actress.nationality}
+                        </div>
+                    </div>
+                ))
+            }
 
-
-        
         </>
     )
 }
