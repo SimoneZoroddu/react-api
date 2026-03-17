92fa9c53634c6311c5e6b1645405e634f7e426f7
SimoneZoroddu <lenviur@gmail.com>
2026-03-17 12:19:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AppHeader from './assets/components/AppHeader'
+import AppMain from './assets/components/AppMain'
+
+function App() {
+
+
+  return (
+    <>
+      <AppHeader />
+      <AppMain />
+    </>
+  )
+}
+
+export default App
